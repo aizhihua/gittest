f4a40482fb8898b5114d208f5c7fca435706af2f
aizhihua <aizhihua@163.com>
2021-09-06 21:40:10 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("helloGit");
+        System.out.println("helloGit3");
     }
 }
