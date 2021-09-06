f75aa85a1b59c3c613c29d02d24e4a7a86207b5b
aizhihua <aizhihua@163.com>
2021-09-06 21:56:45 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("helloworld");
         System.out.println("helloGit");
         System.out.println("helloGit3");
+        System.out.println("helloGit4");
     }
 }
